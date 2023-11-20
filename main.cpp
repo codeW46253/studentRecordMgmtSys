7295d1caa92c56b37d4182d8b5dcfda8f97f8007
CodeW46253 <73611813+codeW46253@users.noreply.github.com>
2023-11-20 14:04:42 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,26 @@
+/*
+ Project Name: Student Record Management System
+ School name : Starlight Highschool
+ Task Requirement:
+      a. Declare  FOUR (4) arrays:
+              studentId   | [int]
+              studentName | [string]
+              scores      | [double]
+              attendance  | [int]
+
+          size: FIFTY (50) student/data
+       b. Create functions:
+              inputStudentData
+              calculateAverageScore
+              displayStudentRecords
+*/
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+
+int main() {
+    return 0;
+}
